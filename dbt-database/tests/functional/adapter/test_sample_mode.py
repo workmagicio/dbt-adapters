4be5fc235f2ecfd7eb52a7614e590893eaf05188
marionettists <marionettists@gmail.com>
2025-10-16 11:23:14 +0800

    feat: init

--- dbt-database/tests/functional/adapter/test_sample_mode.py
+++
@@ -1,7 +0,0 @@
-from dbt.tests.adapter.sample_mode.test_sample_mode import (
-    BaseSampleModeTest,
-)
-
-
-class TestPostgresSampleMode(BaseSampleModeTest):
-    pass
--- dbt-database/tests/functional/functions/test_udfs.py
+++
@@ -1,5 +0,0 @@
-from dbt.tests.adapter.functions.test_udfs import UDFsBasic
-
-
-class TestPostgresUDFs(UDFsBasic):
-    pass
